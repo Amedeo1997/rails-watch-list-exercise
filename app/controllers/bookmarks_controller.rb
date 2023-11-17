ddf8e9b91cdaddd97d91ec557a4edc41b6a81317
ahmed salhi <sirjaizz@gmail.com>
2023-11-17 15:27:39 +0100

    style change

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,28 @@
 class BookmarksController < ApplicationController
-  before_action :find_list, only: [:new, :create]
-
   def new
-    @bookmark = @list.bookmarks.build
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
-    @bookmark = @list.bookmarks.build(bookmark_params)
-
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render 'new'
+      render :new
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to root_path
+
+    redirect_to list_path(@bookmark.list)
   end
 
   private
-
-  def find_list
-    @list = List.find(params[:list_id])
-  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,11 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :destroy]
-
-  def index
+  def  index
     @lists = List.all
   end
 
   def show
-    @bookmark = Bookmark.new
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
   end
 
   def new
@@ -18,20 +17,18 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list)
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
   def destroy
+    @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path, status: :see_other
+
+    redirect_to lists_path
   end
 
   private
-
-  def set_list
-    @list = List.find(params[:id])
-  end
 
   def list_params
     params.require(:list).permit(:name, :photo)
